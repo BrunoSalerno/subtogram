74fa4c1c123861b27623289763f0c5f1874a2c01
bruno <br.salerno@gmail.com>
2016-07-18 21:53:30 -0300

    Db connection fixed

--- app/config/database.rb
+++ app/config/database.rb
@@ -2,6 +2,6 @@ require 'yaml'
 
 config = YAML::load_file('app/config/database.yml')
 
-connection = "postgres://#{config['user']}:#{config['password']}@#{config['host']}:#{config['port']}/#{config['database']}"
+connection = "postgres://#{config['username']}:#{config['password']}@#{config['host']}:#{config['port']}/#{config['database']}"
 
 DB = Sequel.connect(connection)
