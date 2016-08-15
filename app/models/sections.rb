6bb755fa3c1ac37f9e679319fc155e3932c3d50a
bruno <br.salerno@gmail.com>
2016-08-15 19:50:00 -0300

    Section#set_length

--- app/models/sections.rb
+++ app/models/sections.rb
@@ -2,4 +2,11 @@ class Section < Sequel::Model(:sections)
     many_to_one :lines
 
     plugin :geometry
+
+    def set_length
+        # When calculating the length of the section we project the geometry to 
+        # SRID 3857 (Web Mercator)
+        # FIXME Use a local projection to refine the numbers
+        self.length = Sequel.lit("ST_Length(ST_Transform(geometry, 3857))::int")
+    end
 end
