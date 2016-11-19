ed5ead2c12c14062049c5c4c57e625908fbfb05f
Bruno Salerno <br.salerno@gmail.com>
2016-11-19 23:48:40 +0100

    lines_length_by_year and plans_length methods

--- app/helpers/city_helpers.rb
+++ app/helpers/city_helpers.rb
@@ -35,4 +35,35 @@ module CityHelpers
     }
     plans
   end
+
+  def lines_length_by_year(city)
+    lengths = {}
+    years_range = (city.start_year..DateTime.now.year)
+    line_ids = Line.where(city_id: city.id).select_map(:id)
+    Section.where(line_id: line_ids).each do |section|
+      years_range.each do |year|
+        lengths[year] ||= {}
+        line = section.line.url_name
+        if section.buildstart && section.buildstart.to_i <= year && (!section.opening || section.opening.to_i > year)
+          lengths[year][line] ||= {}
+          lengths[year][line][:under_construction] ||= 0
+          lengths[year][line][:under_construction] += section.length
+        elsif section.opening && section.opening.to_i <= year && (!section.closure || section.closure.to_i > year)
+          lengths[year][line] ||= {}
+          lengths[year][line][:operative] ||= 0
+          lengths[year][line][:operative] += section.length
+        end
+      end
+    end
+    lengths
+  end
+
+  def plans_length(city)
+    lengths = {}
+    plan_ids = Plan.where(city_id: city.id).select_map(:id)
+    PlanLine.where(plan_id: plan_ids).each do |line|
+      lengths[line.parent_url_name] = line.length
+    end
+    lengths
+  end
 end
